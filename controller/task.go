f09720459407a87512a913ad8235f55e15f656c1
tokunagawataru <tokunaga.wataru31@gmail.com>
2018-11-25 23:40:42 +0900

    option時のstatus codeを追加

--- controller/task.go
+++ controller/task.go
@@ -120,6 +120,7 @@ func DeleteTask(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 	if r.Method == http.MethodOptions {
+		w.WriteHeader(http.StatusNoContent)
 		return
 	}
 	params := mux.Vars(r)
