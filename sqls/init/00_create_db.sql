92f768ce9e718172693e41f152cba12b59c5a8c9
tokunagawataru <tokunaga.wataru31@gmail.com>
2019-01-06 00:28:12 +0900

    init のsql をDB, table で別にしたsqlの作成

--- sqls/init/00_create_db.sql
+++ sqls/init/00_create_db.sql
@@ -1,14 +1,3 @@
 # mysql -u root < init.sql
 DROP DATABASE IF EXISTS `todoList`;
 CREATE DATABASE `todoList` CHARACTER SET utf8mb4;
-
-CREATE TABLE `todoList`.`tasks` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `uuid` varchar(255) NOT NULL DEFAULT '',
-  `title` varchar(255) NOT NULL DEFAULT '',
-  `detail` varchar(255) DEFAULT '' COMMENT 'titleだけでもOK',
-  `status` varchar(255) NOT NULL DEFAULT 'todo' COMMENT 'todo, in_progress. in_review, done',
-  `created_at` datetime DEFAULT NULL,
-  `updated_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
